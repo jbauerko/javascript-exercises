505d2c2cedc3edf13a9d7a1c521dbc9bfa26fd4d
Julian Bauer-Kong <jbauerkong@gmail.com>
2025-02-28 16:47:10 -0500

    Complete Exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divBy4 = false;
+    let divBy100 = false;
+    let divBy400 = false;
 
+    if (!(year % 4)) {divBy4 = true};
+    if (!(year % 100)) {divBy100 = true};
+    if (!(year % 400)) {divBy400 = true};
+
+    if (!divBy4) {
+        return false;
+    }
+    else if (divBy100 && !divBy400) {
+        return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
