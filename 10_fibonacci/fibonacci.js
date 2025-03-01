80d74b78ac7d947d9c79ccb6eaf0659e67973601
Julian Bauer-Kong <jbauerkong@gmail.com>
2025-02-28 22:37:35 -0500

    Complete Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    // 0, 1, 1, 2, 3, 5, 8, 13... Fibonacci Nums
+    // 0, 1, 2, 3, 4, 5, 6, 7...  Indexes
 
+    n = parseInt(n);
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    if (n === 0) {
+        return 0;
+    }
+    else if (n === 1) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
