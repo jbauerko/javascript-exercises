840082f73214e989b191315ad67d34271d37c51b
Julian Bauer-Kong <jbauerkong@gmail.com>
2025-02-28 21:34:15 -0500

    Complete Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  res = 0;
+  for (let i=0; i<nums.length; i++) {
+    res += nums[i];
+  }
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  product = 1;
+  for (let i=0; i<nums.length; i++) {
+    product *= nums[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+	res = 1;
+  for (let i=0; i<e;i++) {
+    res *= b;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n-1);
 };
 
 // Do not edit below this line
