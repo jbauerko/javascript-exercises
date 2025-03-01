08f39aa9a83ddb90fd2d08b972d6f8da5823ec3f
Julian Bauer-Kong <jbauerkong@gmail.com>
2025-02-28 22:26:56 -0500

    Completed Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Remove any spaces and punctuation
+    string = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    // Split the string into an array, reverse it, then join it back together
+    reversed = string.split('').reverse().join('');
 
+    for (let i=0;i<string.length;i++) {
+        if (string[i] != reversed[i]) {return false;}
+    }
+    return true;
 };
 
 // Do not edit below this line
