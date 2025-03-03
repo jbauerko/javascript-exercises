b2d43ebd21fd98630b43806d8ae8d3df4d88abfa
Julian Bauer-Kong <jbauerkong@gmail.com>
2025-03-02 21:30:30 -0500

    Complete Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
 const findTheOldest = function(people) {
+    let res;
+    let oldest = 0;
+    const d = new Date();
+    for (let i = 0; i < people.length; i++) {
+        let lifespan;
+        if ('yearOfDeath' in people[i]) {
+            lifespan = people[i]['yearOfDeath'] - people[i]['yearOfBirth'];
+        }
+        else {
+            lifespan = d.getFullYear() - people[i]['yearOfBirth'];
+        }
+        if (lifespan > oldest || !oldest) {
+            oldest = lifespan;
+            res = people[i]
+        }
+    }
 
+    return res;
 };
 
 // Do not edit below this line
